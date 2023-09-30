71b826741b7ce506c2f62d55055d4917cf23be87
Sebastian varon <sebastianvaron96@gmail.com>
2023-09-30 22:33:12 +0000

    add function findKey to the proect

---
+++ findKey.js
@@ -0,0 +1,49 @@
+const assertEqual = function (actual, expected) {
+  let message = "";
+  if (actual === expected) {
+    message = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    message = `🛑🛑🛑 Assertion Failed:${actual} !== ${expected}`;
+  }
+  return message;
+};
+
+// function to find the value of certain key if the callback return true from the condition
+const findKey = function (object, callback) {
+  let result;
+  for (let key in object) {
+    let condition = callback(object[key]);
+    if (condition) {
+      result = key;
+      return result;
+    }
+  }
+};
+
+const test = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma");
+const test2 = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 0 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 1 },
+    Akelarre: { stars: 2 },
+  },
+  (x) => x.stars === 2
+);
+console.log(test);
+
+console.log(assertEqual(test, "noma"));
+console.log(assertEqual(test, "Ora"));
+console.log(assertEqual(test2, "Akelarre"));
